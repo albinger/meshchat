8d51080a63dd98aeb1785602e24670d06db408fc
Andrew Mackenzie <andrew@mackenzie-serres.net>
2025-10-06 09:10:30 +0200

    Simple processing of channel information from radio

--- src/device_view.rs
+++ src/device_view.rs
@@ -8,9 +8,13 @@ use crate::device_view::DeviceViewMessage::{DeviceConnect, DeviceDisconnect, Sub
 use crate::{device_subscription, Message, NavigationMessage};
 use iced::futures::channel::mpsc::Sender;
 use iced::futures::SinkExt;
-use iced::widget::{button, container, text, Column};
+use iced::widget::{button, container, text, Column, Row};
 use iced::{Element, Length, Task};
 use iced_futures::Subscription;
+use meshtastic::protobufs::channel::Role;
+use meshtastic::protobufs::channel::Role::*;
+use meshtastic::protobufs::from_radio::PayloadVariant;
+use meshtastic::protobufs::Channel;
 use meshtastic::utils::stream::BleId;
 
 #[derive(Clone)]
@@ -31,6 +35,7 @@ pub enum DeviceViewMessage {
 pub struct DeviceView {
     pub connection_state: ConnectionState,
     subscription_sender: Option<Sender<SubscriberMessage>>, // TODO Maybe combine with Disconnected state?
+    channels: Vec<Channel>, // Upto 8 - but maybe depends on firmware
 }
 
 async fn request_connection(mut sender: Sender<SubscriberMessage>, id: BleId) {
@@ -48,6 +53,7 @@ impl DeviceView {
         Self {
             connection_state: Disconnected,
             subscription_sender: None,
+            channels: vec![],
         }
     }
 
@@ -59,14 +65,14 @@ impl DeviceView {
     pub fn update(&mut self, device_event: DeviceViewMessage) -> Task<Message> {
         match device_event {
             DeviceConnect(id) => {
-                self.connection_state = Connecting(id.clone()); // TODO make state change depend on message back from subscriptino
+                self.connection_state = Connecting(id.clone()); // TODO make state change depend on message back from subscription
                 let sender = self.subscription_sender.clone();
                 Task::perform(request_connection(sender.unwrap(), id), |_| {
                     Message::Navigation(NavigationMessage::Connecting)
                 })
             }
             DeviceDisconnect(id) => {
-                self.connection_state = Disconnecting(id.clone()); // TODO make state change depend on message back from subscriptino
+                self.connection_state = Disconnecting(id.clone()); // TODO make state change depend on message back from subscription
                 // Send a message to the subscription to disconnect
                 let sender = self.subscription_sender.clone();
                 Task::perform(request_disconnection(sender.unwrap()), |_| {
@@ -83,12 +89,28 @@ impl DeviceView {
                     Task::perform(empty(), |_| Message::Navigation(NavigationMessage::Back))
                 }
                 Ready(sender) => {
-                    println!("Subscriber is ready");
                     self.subscription_sender = Some(sender);
                     Task::none()
                 }
                 DevicePacket(packet) => {
-                    println!("Received packet: {:?}", packet);
+                    match packet.payload_variant.unwrap() {
+                        PayloadVariant::Packet(_) => {}
+                        PayloadVariant::MyInfo(_) => {}
+                        PayloadVariant::NodeInfo(_) => {}
+                        PayloadVariant::Config(_) => {}
+                        PayloadVariant::LogRecord(_) => {}
+                        PayloadVariant::ConfigCompleteId(_) => {}
+                        PayloadVariant::Rebooted(_) => {}
+                        PayloadVariant::ModuleConfig(_) => {}
+                        PayloadVariant::Channel(channel) => self.channels.push(channel),
+                        PayloadVariant::QueueStatus(_) => {}
+                        PayloadVariant::XmodemPacket(_) => {}
+                        PayloadVariant::Metadata(_) => {}
+                        PayloadVariant::MqttClientProxyMessage(_) => {}
+                        PayloadVariant::FileInfo(_) => {}
+                        PayloadVariant::ClientNotification(_) => {}
+                        PayloadVariant::DeviceuiConfig(_) => {}
+                    }
                     Task::none()
                 }
             },
@@ -121,12 +143,36 @@ impl DeviceView {
             }
         }
 
+        for channel in &self.channels {
+            let channel_row = match Role::try_from(channel.role).unwrap() {
+                Disabled => break,
+                Primary => Self::channel_row(true, channel),
+                Secondary => Self::channel_row(false, channel),
+            };
+            main_col = main_col.push(channel_row);
+        }
+
         let content = container(main_col)
             .height(Length::Fill)
             .width(Length::Fill)
             .align_x(iced::alignment::Horizontal::Left);
 
         content.into()
+    }
+
+    fn channel_row(primary: bool, channel: &Channel) -> Row<'static, Message> {
+        let mut channel_row = Row::new();
+        if primary {
+            channel_row = channel_row.push(text("Primary: "))
+        } else {
+            channel_row = channel_row.push(text("Secondary: "))
+        }
+
+        if let Some(settings) = &channel.settings {
+            channel_row = channel_row.push(text(settings.name.clone()));
+        }
+
+        channel_row
     }
 
     /// Create subscriptions for events coming from a connected hardware device (radio)
